3df8c63eed91824c42cea7b38e5a8e063b0fb8eb
aniketpai0209 <aniketpai.0209@gmail.com>
2022-07-15 01:43:22 +0530

    User Interface Commit

--- app/src/main/java/com/codeist/birthdaygreet/MainActivity.kt
+++ app/src/main/java/com/codeist/birthdaygreet/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.codeist.birthdaygreet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun createBirthdayCard(view: View) {
+        val name = findViewById<EditText?>(R.id.nameInput).editableText.toString()
+
+        Toast.makeText(this, "Name is $name", Toast.LENGTH_LONG).show()
     }
 }
